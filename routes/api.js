0d1491de2c8c43366804c52dd5982f8db8afa6e2
saissei <tariraritarara.214@gmil.com>
2019-05-01 01:37:37 +0900

    コードのリファクタリングとコードフォーマットを実施

--- routes/api.js
+++ routes/api.js
@@ -1,36 +1,37 @@
-const express = require('express');
-const router = express.Router();
-const geth = require('../public/javascripts/nodejs/connect_geth')
+const express = require('express')
+const router = express.Router()
+const geth = require('../lib/connect_geth')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.status(200).header('Content-Type', 'application/json; charset=utf-8').json({status: "success"});
-});
+  res
+    .status(200)
+    .header('Content-Type', 'application/json; charset=utf-8')
+    .json({ status: 'success' })
+})
 
-router.get('/accounts', function(req, res, next){
-  geth.getAccounts().then(function(_account){
-    res.status(200).header('Content-Type', 'application/json; charset=utf-8').json({accounts: _account});
+router.get('/accounts', function(req, res, next) {
+  geth.getAccounts().then(function(_account) {
+    res
+      .status(200)
+      .header('Content-Type', 'application/json; charset=utf-8')
+      .json({ accounts: _account })
   })
 })
 
-router.post('/send-coin', function(req, res, next){
-  const body = req.body;
-  console.log(body);
-  geth.connector().methods.transfer(body.to, body.value).send({
-    from: body.from
-  }, (error, transactionHash) => {
-    if(error){ console.log(error); process.exit() };
+router.post('/send-coin', async (req, res, next) => {
+  const body = req.body
+  geth
+    .oreTransfer(body.from, body.to, body.value)
+    .then(async transactionHash => {
+      geth
+        .getTransaction(transactionHash)
+        .then(async blockNum => {
+          let from_coin = await geth.OreBalance(body.from)
+          let to_coin = await geth.OreBalance(body.from)
 
-    geth.getTransaction(transactionHash)
-    .then(blockNum => {
-      geth.OreBalance(body.from)
-      .then(from_oc => {
-        geth.OreBalance(body.to)
-        .then(to_oc => {
-
-
-          const from_coin = parseInt((from_oc._hex).replace(/^0x/, ''), 16);
-          const to_coin = parseInt((to_oc._hex).replace(/^0x/, ''), 16);
+          from_coin = parseInt(from_coin._hex.replace(/^0x/, ''), 16)
+          to_coin = parseInt(to_coin._hex.replace(/^0x/, ''), 16)
           const data = {
             transactionHash: transactionHash,
             from: body.from,
@@ -39,33 +40,38 @@ router.post('/send-coin', function(req, res, next){
             send_coin: body.value,
             to_coin: to_coin
           }
-          console.log(data);
-          res.status(200).header('Content-Type', 'application/json; charset=utf-8').json(data);
+          console.log(data)
+          res
+            .status(200)
+            .header('Content-Type', 'application/json; charset=utf-8')
+            .json(data)
         })
         .catch(err => {
           console.error(err)
-          res.status(400).header('Content-Type', 'application/json; charset=utf-8').json({status: 'send error'});
+          res
+            .status(400)
+            .header('Content-Type', 'application/json; charset=utf-8')
+            .json({ status: 'send error' })
         })
-      })
-      .catch(err => {
-        console.error(err)
-        res.status(400).header('Content-Type', 'application/json; charset=utf-8').json({status: 'send error'});
-      })
     })
     .catch(err => {
       console.error(err)
-      res.status(400).header('Content-Type', 'application/json; charset=utf-8').json({status: 'send error'});
+      res
+        .status(400)
+        .header('Content-Type', 'application/json; charset=utf-8')
+        .json({ status: 'send error' })
     })
-  })
-});
+})
 
-router.get('/balance/:id', function(req, res, next){
-  const from = req.params.id;
-  geth.OreBalance(from)
-  .then(resp => {
-    const coin = parseInt((resp._hex).replace(/^0x/, ''), 16)
-    res.status(200).header('Content-Type', 'application/json; charset=utf-8').json({coin: coin});
+router.get('/balance/:id', function(req, res, next) {
+  const from = req.params.id
+  geth.OreBalance(from).then(resp => {
+    const coin = parseInt(resp._hex.replace(/^0x/, ''), 16)
+    res
+      .status(200)
+      .header('Content-Type', 'application/json; charset=utf-8')
+      .json({ coin: coin })
   })
 })
 
-module.exports = router;
+module.exports = router
